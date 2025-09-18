99b24034e710fa88666e0072d8c56a8d543a612a
Simhadriappanna <simhadriappanna04@gmail.com>
2025-09-18 15:14:05 -0400

    change

--- index.js
+++ index.js
@@ -1,40 +1,31 @@
-function getweather(){
-    let city=document.getElementById("city")
-
-showweather(city.value)
+function getweather() {
+    let city = document.getElementById("city");
+    showweather(city.value);
 }
-async function showweather(city){
-    const url = `https://open-weather13.p.rapidapi.com/city?city=${city}&lang=EN`;
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': 'f570cb2952msh53639550a3441acp177a73jsn58461091888f',
-		'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
-	}
-};
 
-try {
-	const response = await fetch(url, options);
+async function showweather(city) {
+    const url = `https://weather-api138.p.rapidapi.com/weather?city_name=${city}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'f570cb2952msh53639550a3441acp177a73jsn58461091888f',
+            'x-rapidapi-host': 'weather-api138.p.rapidapi.com'
+        }
+    };
 
-    if(!response.ok){
-    throw new Error("weather not display")
-
-} 
-	const result = await response.json()
-   getdata(result)
-} catch (error) {
-	console.error(error);
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json(); 
+        getdata(result);
+    } catch (error) {
+        console.error(error);
+    }
 }
-}
-function getdata(result){
-    let item=document.getElementById('main')
-    item.innerHTML=
-    ` <p>city:${result.name}</p>
-    <p>temperature:${result.main.temp}</p>
 
-    `
-  
+function getdata(result) {
+    let item = document.getElementById('main');
 
-
-    
+    item.innerHTML =
+        `<p>City: ${result.name}</p>
+         <p>Temperature: ${result.main.temp} °C</p>`;
 }
